69ceec52b50302ef6545af294b89d75bfecaa057
Your Name <you@example.com>
2022-03-20 15:30:29 +0100

    upload a post file api

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const mongoose = require('mongoose');
 const dotenv   = require('dotenv');
 const helmet   = require('helmet');
 const morgan   = require('morgan');
+const multer = require("multer");
+const path = require("path");
+//const router = express.Router();
 const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/posts');
+
 
 
 
@@ -16,9 +20,35 @@ mongoose.connect(process.env.Mongo_URL, {useNewUrlParser: true,useUnifiedTopolog
     console.log("Connected to MONGODB");
 });
 
+// to  not make any request , just using the folder::
+app.use("/postPictures", express.static(path.join(__dirname, "public/postPictures")));
+
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "public/postPictures");
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.req.body.name);
+    },
+  });
+
+const upload = multer({ storage: storage }); //multer : midlleware for uploading files
+
+//uploading files:
+app.post("/api/upload",upload.single("file"),(req,res)=>{
+    try{
+        return res.status(200);
+    }
+    catch(err){
+       console.log(err);
+    }
+});
+
 
 app.use("/api/users" ,userRoute);
 app.use("/api/auth" ,authRoute);
