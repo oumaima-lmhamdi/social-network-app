0aa9024692c6680345f65de57fe89bdf4ce3871b
Your Name <you@example.com>
2022-04-09 22:09:41 +0000

    adding chat

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,269 @@
+import React from 'react'
+import TopBar from "../../components/topBar/TopBar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import { Search, Person, ChatBubble, Notifications,GroupAdd,Logout } from '@mui/icons-material';
+import {Link} from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
+
+import Rightbar from "../../components/rightbar/Rightbar";
+import Feed from "../../components/feed/Feed";
+import FollowedFriend from "../../components/followedFriend/FollowedFriend";
+
+import axios from "axios";
+import Post from '../../components/post/Post'
+import { useContext, useState } from "react";
+
+import "./home.css";
+import AddFriend from '../../components/addFriend/AddFriend';
+import SearchedUser from '../../components/searchedUser/SearchedUser';
+
+
+
+
+
+export default function Home() {
+  const {user,dispatch} = useContext(AuthContext);
+  const [searchfield,setsearchfield] = useState("");
+  const [searchedPosts,setsearchedPosts] = useState([]);
+  const [followings,setFollowings] = useState([]);
+  const [searchedUsers,setsearchedUsers] = useState([]);
+
+
+
+
+  const handleChange = e => {
+    setsearchfield(e.target.value);
+  };
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    try{ 
+      
+      const res1 = await axios.get(`/posts?desc=${searchfield}`);
+      const res2 = await axios.get(`/users/search?username=${searchfield}`);
+
+      console.log(res1.data);
+      console.log(res2.data);
+
+      setsearchedPosts(
+        res1.data.sort((post1, post2) => {
+          return new Date(post2.createdAt) - new Date(post1.createdAt);
+        })
+      );
+      setsearchedUsers(res2.data);
+      
+     }
+    catch(err){
+      console.log(err);
+    }
+    
+
+  } 
+
+  
+
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const logoutCall = async (dispatch) => {
+    dispatch({ type: "LOGOUT" });
+  };
+  const handleLogoutClick = () => {
+    logoutCall(
+      dispatch
+    );
+  };
+  const handlePeopleFilter = () => {
+    setsearchedPosts([]);
+    
+    
+  };
+  const handlePostsFilter = () => {
+    setsearchedUsers([]);
+  };
+
+  
+
+  if (searchfield) {
+
+    return (
+      <>
+      <div className="topbarContainer">
+      <div className="topbarLeft">
+      <Link to="/" style={{textDecoration:"none"}}>
+        <span className="logo">Social Network</span>
+      </Link>
+
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <form onSubmit={handleSearch}>
+          <input
+            placeholder="Friends or posts"
+            type="search"
+            className="searchInput"
+            onChange={handleChange}
+          />
+          
+          </form>
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          
+          <Link to="/" style={{textDecoration:"none",color:"white"}}>
+          <span className="topbarLink">My Feed</span>
+      </Link>
+      <Link to="/messenger" style={{textDecoration:"none",color:"white"}}>
+          <span className="topbarLink">My Chats</span>
+      </Link>
+      <Link to="/" style={{textDecoration:"none",color:"white"}}>
+          
+          <button className="logout" onClick={handleLogoutClick} >Logout</button>
+          </Link>
+
+        </div>
+        <div className="topbarIcons">
+          
+          <div className="topbarIconItem">
+            <ChatBubble />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        
+        <Link to={`/profile/${user.username}`}>
+        <img
+            src={
+              user.profilePicture
+                ? PF + user.profilePicture
+                : PF + "person/noAvatar.png"
+            }
+            alt=""
+            className="topbarImg"
+          />
+        </Link>
+
+      </div>
+    </div>
+      
+      <div className="container">
+              <Sidebar/> 
+          
+      <div className="feed">
+      <div className="feedWrapper">
+        <div className="filterDiv">
+          <button className="filterButton" id="peopleBtn" onClick={handlePeopleFilter}>People</button>
+          <button className="filterButton" onClick={handlePostsFilter}>Posts</button>
+        </div>
+
+      <div className="feedAddFriend">
+      {
+        searchedUsers.filter(item => (user.following.includes(item._id))).map((u) => (
+          <FollowedFriend key={u.id} user={u}/>
+
+        ))}
+      </div>
+
+      <div className="feedAddFriend">
+      {
+        searchedUsers.filter(item => (item.username !== user.username)).filter(item => (!(user.following.includes(item._id)))).map((u) => (
+          <SearchedUser key={u.id} user={u}/>
+
+        ))}
+      </div>
+      <hr />
+        
+      
+      {
+      searchedPosts.map((p) => (
+        <Post key={p._id} post={p} className="searchedPost"/>
+      ))}
+      </div>
+      </div>
+              
+              <Rightbar/>
+              
+       </div>
+    </>
+
+    )
+  }
+ ///////////////////////////
+    else{
+    return (
+        <div>
+        <>
+        <div className="topbarContainer">
+      <div className="topbarLeft">
+      <Link to="/" style={{textDecoration:"none"}}>
+        <span className="logo">Social Network</span>
+      </Link>
+
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <form onSubmit={handleSearch}>
+          <input
+            placeholder="Friends or posts"
+            type="search"
+            className="searchInput"
+            onChange={handleChange}
+          />
+          
+          </form>
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          
+          <Link to="/" style={{textDecoration:"none",color:"white"}}>
+          <span className="topbarLink">My Feed</span>
+      </Link>
+      <Link to="/messenger" style={{textDecoration:"none",color:"white"}}>
+          <span className="topbarLink">My Chats</span>
+      </Link>
+      <Link to="/" style={{textDecoration:"none",color:"white"}}>
+          
+          <button className="logout" onClick={handleLogoutClick} >Logout</button>
+          </Link>
+
+        </div>
+        <div className="topbarIcons">
+          
+          <div className="topbarIconItem">
+            <ChatBubble />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        
+        <Link to={`/profile/${user.username}`}>
+        <img
+            src={
+              user.profilePicture
+                ? PF + user.profilePicture
+                : PF + "person/noAvatar.png"
+            }
+            alt=""
+            className="topbarImg"
+          />
+        </Link>
+
+      </div>
+    </div>
+            <div className="container">
+              <Sidebar/>
+              <Feed/>
+              <Rightbar/>
+              
+            </div>
+        </>
+        </div>)}
+}
---
+++ frontend/src/pages/messenger/Messenger.jsx
@@ -0,0 +1,233 @@
+import "./messenger.css";
+import TopBar from "../../components/topBar/TopBar";
+import Conversation from "../../components/conversations/Conversation";
+import Message from "../../components/message/Message";
+import ChatOnline from "../../components/chatOnline/ChatOnline";
+import { useContext, useEffect, useRef, useState } from "react";
+import CallIcon from '@mui/icons-material/Call';
+import VideoCallIcon from '@mui/icons-material/VideoCall';
+import {Link} from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
+import axios from "axios";
+import { io } from "socket.io-client";
+
+export default function Messenger() {
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const [friends, setFriends] = useState([]);
+  const [conversations, setConversations] = useState([]);
+  const [currentChat, setCurrentChat] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+  const [arrivalMessage, setArrivalMessage] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const [receiver, setReceiver] = useState({});
+  const socket = useRef();
+  const { user } = useContext(AuthContext);
+  const scrollRef = useRef();
+  const currentId= user._id;
+
+  useEffect(() => {
+    socket.current = io("ws://localhost:8900");
+    socket.current.on("getMessage", (data) => {
+      setArrivalMessage({
+        sender: data.senderId,
+        text: data.text,
+        createdAt: Date.now(),
+      });
+      
+      
+    });
+  }, []);
+
+  useEffect(() => {
+    const getFriends = async () => {
+      const res = await axios.get("/users/friends/" + currentId);
+      setFriends(res.data);
+    };
+
+    getFriends();
+  }, [currentId]);
+
+  useEffect(() => {
+    arrivalMessage &&
+      currentChat?.members.includes(arrivalMessage.sender) &&
+      setMessages((prev) => [...prev, arrivalMessage]);
+  }, [arrivalMessage, currentChat]);
+
+  useEffect(() => {
+    socket.current.emit("addUser", user._id);
+    socket.current.on("getUsers", (users) => {
+      setOnlineUsers(
+        user.following.filter((f) => users.some((u) => u.userId === f))
+      );
+    });
+  }, [user]);
+
+  useEffect(() => {
+    const getConversations = async () => {
+      try {
+        const res = await axios.get("/conversations/" + user._id);
+        setConversations(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getConversations();
+  }, [user._id]);
+
+  useEffect(() => {
+    const getMessages = async () => {
+      try {
+        const res = await axios.get("/messages/" + currentChat?._id);
+        setMessages(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getMessages();
+  }, [messages]);
+
+  useEffect(()=> {
+    if(currentChat){
+
+      const func = async() => {
+        try {
+          const recId = currentChat.members.find(
+            (member) => member !== user._id
+          );
+          const receiver = await axios.get("/users?userId=" + recId);
+          //console.log("receiver is" + receiver );
+          setReceiver(receiver.data);
+        } catch (err) {
+          console.log(err);
+        }
+  
+      }
+      func();
+    }
+  },[currentChat]);
+
+  
+  useEffect(() => {
+    
+  });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    //if statement for not sending empty message.
+    if (newMessage){
+    const message = {
+      sender: user._id,
+      text: newMessage,
+      conversationId: currentChat._id,
+    };
+
+    const receiverId = currentChat.members.find(
+      (member) => member !== user._id
+    );
+    
+    socket.current.emit("sendMessage", {
+      senderId: user._id,
+      receiverId,
+      text: newMessage,
+    });
+    
+
+    try {
+      const res = await axios.post("/messages", message);
+      setMessages([...messages, res.data]);
+      setNewMessage("");
+    } catch (err) {
+      console.log(err);
+    }};
+  };
+
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+  return (
+    <>
+      <TopBar />
+      <div className="messenger">
+        <div className="chatMenu">
+          <div className="chatMenuWrapper">
+            
+            {conversations.map((c) => (
+              <div onClick={() => setCurrentChat(c)}>
+                <Conversation conversation={c} currentUser={user} />
+                <hr />
+              </div>
+              
+            ))}
+          </div>
+        </div>
+        <div className="chatBox">
+          <div className="chatBoxWrapper">
+            {currentChat ? (
+              <>
+        <div className="topOfchat">
+        <div className="picAndName">
+        <Link to={`/profile/${receiver.username}`}>
+
+      <img
+        className="conversationImg"
+        src={
+          receiver?.profilePicture
+            ? PF + receiver.profilePicture
+            : PF + "person/noAvatar.png"
+        }
+        alt=""
+      />
+          </Link>
+
+      <span className="conversationName">{receiver?.username}</span>
+      </div>
+      <div className="callDiv">
+      <CallIcon htmlColor="blue" className="callIcon" />
+      <VideoCallIcon htmlColor="blue" className="callIcon" />
+      </div>
+    </div>
+    <hr />
+                <div className="chatBoxTop">
+                  
+                  
+                  {
+
+                  messages.map((m) => (
+                    <div ref={scrollRef}>
+                      <Message message={m} own={m.sender === user._id}  />
+                    </div>
+                  ))}
+                </div>
+                <div className="chatBoxBottom">
+                  <textarea
+                    className="chatMessageInput"
+                    placeholder="Message..."
+                    onChange={(e) => setNewMessage(e.target.value)}
+                    value={newMessage}
+                  ></textarea>
+                  <button className="chatSubmitButton" onClick={handleSubmit}>
+                    Send
+                  </button>
+                </div>
+              </>
+            ) : (
+              <span className="noConversationText">
+                Select a conversation to enter the chat.
+              </span>
+            )}
+          </div>
+        </div>
+        <div className="chatOnline">
+          <div className="chatOnlineWrapper">
+            <ChatOnline
+              onlineUsers={onlineUsers}
+              currentId={user._id}
+              setCurrentChat={setCurrentChat}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
